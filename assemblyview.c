87056f0248cd17fdca9fe361a89ac7bbe19fef2d
naffer <cfanning43@gmail.com>
2024-02-26 11:43:47 -0500

    added test loop

---
+++ assemblyview.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+
+int i;
+
+for(i=0; i < 10; i++) // Loop 10 times.
+{
+puts("i\n"); // put the string to the output.
+i = i * 2
+}
+return 0; // Tell OS the program exited without errors.
+}
